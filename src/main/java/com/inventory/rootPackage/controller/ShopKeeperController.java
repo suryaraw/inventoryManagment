06579bd70c637ba9b532b7bfd4817bdc3c15be82
maniShjavacoder <manigapathy30@gmail.com>
2025-09-11 11:53:39 +0530

    jsp modified

--- src/main/java/com/inventory/rootPackage/controller/ShopKeeperController.java
+++ src/main/java/com/inventory/rootPackage/controller/ShopKeeperController.java
@@ -45,6 +45,4 @@ public class ShopKeeperController {
     		return service.picked(request, model);
     }
     
-    
-    
 }
--- src/main/java/com/inventory/rootPackage/model/Item.java
+++ src/main/java/com/inventory/rootPackage/model/Item.java
@@ -1,11 +1,15 @@
 package com.inventory.rootPackage.model;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,7 +27,7 @@ public class Item {
 	@Column(name = "item_id")
 	private long id;
 	private String name;
-	//private String category;
+//	private String category;
 	@ManyToOne
     @JoinColumn(name = "category_id") 
     private Category category;
@@ -32,5 +36,29 @@ public class Item {
 	private Double wholesalePrice;
 	private Double RetailPrice;
 	private Double gstRate;
+//    private LocalDate dateOfPurchase;
+//    @ManyToMany
+//    @JoinTable
+//    private Wholesaler supplier;
+	
+//	@Id
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "item_seq")
+//   // @SequenceGenerator(name = "item_seq", sequenceName = "item_sequence", initialValue = 1001, allocationSize = 1)
+//    private Long itemId;
+//
+//    private String itemName;
+//    private String category;
+//    private String description;
+//    private Integer quantityInStock;
+////    private String unitOfMeasure;
+////    private String location;
+//    @ManyToMany
+//    @JoinTable
+//    private Wholesaler supplier;
+//    private Double purchasePrice;
+//    private Double sellingPrice;
+//    private LocalDate dateOfPurchase;
+////    private Integer reorderLevel;
+////    private Integer reorderQuantity;
 	
 }
